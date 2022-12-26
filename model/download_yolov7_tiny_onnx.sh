a0026526f1428655affd4eceff33b37bf7954654
Tatsuya FUKUOKA <82201896+tatsuya-fukuoka@users.noreply.github.com>
2022-12-26 22:09:15 +0900

    Update download_yolov7_tiny_onnx.sh

--- model/download_yolov7_tiny_onnx.sh
+++ model/download_yolov7_tiny_onnx.sh
@@ -1,5 +1,5 @@
 FILE_ID=1-P3RpFnbXv_a99EW_wiBCUcZ7ssJzEnj
 FILE_NAME=yolov7-tiny.onnx
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=${FILE_ID}" > /dev/null
-CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=${FILE_ID}" -o ${FILE_NAME}
+CONFIRM=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate "https://drive.google.com/uc?export=download&id=${FILE_ID}" -O- | sed -En 's/.*confirm=([0-9A-Za-z_]+).*/\1/p');
+wget --load-cookies /tmp/cookies.txt "https://drive.google.com/uc?export=download&confirm=${CONFIRM}&id=${FILE_ID}" -O ${FILE_NAME};
+rm -f /tmp/cookies.txt
