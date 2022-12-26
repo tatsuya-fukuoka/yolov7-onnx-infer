e55e8512e8cd8062fc59d9064bc360e88890177d
Tatsuya FUKUOKA <82201896+tatsuya-fukuoka@users.noreply.github.com>
2022-12-26 22:20:18 +0900

    Update download_yolov7_tiny_onnx.sh

--- model/download_yolov7_tiny_onnx.sh
+++ model/download_yolov7_tiny_onnx.sh
@@ -1,3 +1,3 @@
-FILE_ID="1-P3RpFnbXv_a99EW_wiBCUcZ7ssJzEnj"
-FILE_NAME="yolov7-tiny.onnx"
-wget --no-check-certificate "http://drive.google.com/uc?export=view&id=$FILE_ID" -O $FILE_NAME
+FILE_ID="1-P3RpFnbXv_a99EW_wiBCUcZ7ssJzEnj";
+FILE_NAME="yolov7-tiny.onnx";
+wget --no-check-certificate "http://drive.google.com/uc?export=view&id=$FILE_ID" -O $FILE_NAME;
